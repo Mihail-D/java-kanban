2e14579cdc91890468c9fa118b2c09e19ee676de
Nuby <nuby.4register@gmail.com>
2023-02-02 15:24:56 +0300

    готово к первому ревью

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
+
     static InMemoryTaskManager taskManager;
     static InMemoryHistoryManager inMemoryHistoryManager;
     static FileBackedTasksManager fileBackedTasksManager;
@@ -41,49 +42,34 @@ public class Main {
         String taskStatus;
 
         while (true) {
-            System.out.println("MENU"); // TODO
             item = scanner.nextInt();
 
             switch (item) {
                 case 1:
-                    System.out.println("taskTitle"); // TODO
                     taskTitle = scanner.next();
-                    System.out.println("taskDescription"); // TODO
                     taskDescription = scanner.next();
-                    System.out.println("mode taskMode epicMode subTaskMode"); // TODO
                     mode = scanner.next();
 
                     if (mode.equals("taskMode") || mode.equals("epicMode")) {
                         fileBackedTasksManager.dataAdd(taskTitle, taskDescription, mode);
                     }
                     else if (mode.equals("subTaskMode")) {
-                        System.out.println("parentKey"); // TODO
-
                         parentKey = scanner.next();
                         fileBackedTasksManager.dataAdd(taskTitle, taskDescription, mode, parentKey);
                     }
 
                     break;
                 case 2:
-                    System.out.println("taskKey"); // TODO
                     taskKey = scanner.next();
-
-                    System.out.println("taskTitle"); // TODO
                     taskTitle = scanner.next();
-
-                    System.out.println("taskDescription"); // TODO
                     taskDescription = scanner.next();
 
                     if (taskKey.charAt(0) == 's') {
-                        System.out.println("parentKey"); // TODO
                         parentKey = scanner.next();
-
-                        System.out.println("taskStatus"); // TODO
                         taskStatus = scanner.next();
                         fileBackedTasksManager.dataEdit(taskKey, taskTitle, taskDescription, taskStatus, parentKey);
                     }
                     else if (taskKey.charAt(0) == 't') {
-                        System.out.println("taskStatus"); // TODO
                         taskStatus = scanner.next();
                         fileBackedTasksManager.dataEdit(taskKey, taskTitle, taskDescription, taskStatus);
                     }
@@ -93,30 +79,21 @@ public class Main {
                     break;
 
                 case 3:
-                    System.out.println("taskKey"); // TODO
                     taskKey = scanner.next();
                     fileBackedTasksManager.dataGet(taskKey);
                     break;
                 case 4:
                     taskManager.collectAllTasks();
-                    System.out.println(taskManager.collectAllTasks()); // TODO
                     break;
                 case 5:
-                    System.out.println("taskKey"); // TODO
                     taskKey = scanner.next();
-
-                    System.out.println(taskManager.collectEpicSubtasks(taskKey)); // TODO
                     taskManager.collectEpicSubtasks(taskKey);
-
                     break;
                 case 6:
-                    System.out.println("taskKey"); // TODO
                     taskKey = scanner.next();
 
                     if (taskKey.charAt(0) == 's') {
-                        System.out.println("parentKey"); // TODO
                         parentKey = scanner.next();
-
                         fileBackedTasksManager.dataDelete(taskKey, parentKey);
                     }
                     else {
