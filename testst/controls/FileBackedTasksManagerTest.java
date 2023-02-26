85ba0bee5ae00173aaf89920cf2fd0ad1d32bf84
Nuby <nuby.4register@gmail.com>
2023-02-26 17:16:31 +0300

    Tests new 4

---
+++ testst/controls/FileBackedTasksManagerTest.java
@@ -0,0 +1,53 @@
+package controls;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tasks.Task;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static tasks.TaskStages.NEW;
+import static tasks.TaskTypes.TASK;
+
+public class FileBackedTasksManagerTest extends TaskManagerTest<InMemoryTaskManager> {
+    FileBackedTasksManager fileManager;
+
+    private File dataFile;
+    private Path dataPath;
+    private Path historyPath;
+    private File historyFile;
+
+    @BeforeEach
+    void createFileBackedTasksManagerTest() {
+        dataPath = Path.of("dataFile.csv");
+        dataFile = new File(String.valueOf(dataPath));
+
+        historyPath = Path.of("historyFile.csv");
+        historyFile = new File(String.valueOf(historyPath));
+
+        //taskManager = new FileBackedTasksManager(dataFile, historyFile);
+    }
+
+    @Test
+    public void getInitNumber() {
+        assertEquals(0, InMemoryTaskManager.getTasksStorage().size());
+        fileManager = new FileBackedTasksManager(dataFile, historyFile);
+        System.out.println(InMemoryTaskManager.getTasksStorage());
+        assertEquals(5, InMemoryTaskManager.getTasksStorage().size());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy_HH:mm");
+        Task task3 = new Task("task_5", "description_5", false, NEW, TASK,
+                LocalDateTime.parse("22.03.2023_17:00", formatter), Duration.ofMinutes(60)
+        );
+
+        InMemoryTaskManager taskManager = new InMemoryTaskManager();
+        taskManager.taskAdd(task3);
+        System.out.println("=====================================================");
+        System.out.println(InMemoryTaskManager.getTasksStorage());
+    }
+
+}
---
+++ testst/controls/InMemoryTaskManagerTest.java
@@ -0,0 +1,7 @@
+package controls;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryTaskManagerTest  extends TaskManagerTest<InMemoryTaskManager> {
+
+}
