9ee5a8e7b279cde269c95d5a41eee8a96bb96e6e
Nuby <nuby.4register@gmail.com>
2023-02-28 21:54:33 +0300

    FileBackedTasksManager 1.9

--- testst/controls/FileBackedTasksManagerTest.java
+++ testst/controls/FileBackedTasksManagerTest.java
@@ -116,6 +116,68 @@ public class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksM
     }
 
     @Test
+    public void shouldSaveEmptyTasks() throws IOException {
+        Path path = Path.of("./src/data/dataFile.csv");
+        assertEquals(0, Files.size(path));
+
+        if (dataFile.exists() && !dataFile.isDirectory()) {
+
+            try (final BufferedWriter writer = new BufferedWriter((new FileWriter(dataFile, UTF_8)))) {
+
+
+                for (String entry : taskManager.getTasksStorage().keySet()) {
+                    writer.append(taskManager.getTaskFormattedData(taskManager.getTasksStorage().get(entry).getTaskId()));
+                    writer.newLine();
+                }
+
+            } catch (IOException e) {
+                throw new ManagerSaveException("Не удалось сохранить данные задач");
+            }
+
+            assertEquals(0, Files.size(path));
+        }
+    }
+
+    @Test
+    public void shouldSaveEmptyEpic() throws IOException {
+        assertEquals(0, Files.size(Path.of("./src/data/dataFile.csv")));
+
+        taskManager.epicAdd(epic);
+        String savedTaskKey = epic.getTaskId();
+        String restoredTaskKey = null;
+
+        assertEquals(0, epic.relatedSubTask.size());
+
+        if (dataFile.exists() && !dataFile.isDirectory()) {
+
+            try (final BufferedWriter writer = new BufferedWriter((new FileWriter(dataFile, UTF_8)))) {
+
+
+                for (String entry : taskManager.getTasksStorage().keySet()) {
+                    writer.append(taskManager.getTaskFormattedData(taskManager.getTasksStorage().get(entry).getTaskId()));
+                    writer.newLine();
+                }
+
+            } catch (IOException e) {
+                throw new ManagerSaveException("Не удалось сохранить данные задач");
+            }
+
+            try (BufferedReader br = new BufferedReader(new FileReader(dataFile))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if (line.trim().isEmpty()) {
+                        continue;
+                    }
+                    String[] tokens = line.split(",");
+                    restoredTaskKey = tokens[0];
+                }
+            }
+
+            assertEquals(restoredTaskKey, savedTaskKey);
+        }
+    }
+
+    @Test
     public void shouldSaveHistory() throws IOException {
         assertEquals(0, Files.size(Path.of("./src/data/historyFile.csv")));
         taskManager.taskAdd(task1);
@@ -166,6 +228,20 @@ public class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksM
     }
 
     @Test
+    public void shouldRestoreEmptyTasks() throws IOException {
+        FileBackedTasksManager newTaskManager = new FileBackedTasksManager(dataFile, historyFile);
+
+        BufferedWriter writer = Files.newBufferedWriter(dataFile.toPath());
+        writer.write("");
+        writer.flush();
+
+        assertEquals(0, Files.size(dataFile.toPath()));
+
+        newTaskManager.restoreTasks(dataFile);
+        assertEquals(0, FileBackedTasksManager.getTaskManager().getTasksStorage().size());
+    }
+
+    @Test
     public void shouldRestoreHistory() throws IOException {
         FileBackedTasksManager newTaskManager = new FileBackedTasksManager(dataFile, historyFile);
         assertEquals(0, Files.size(historyFile.toPath()));
@@ -180,6 +256,18 @@ public class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksM
 
         newTaskManager.restoreTasks(historyFile);
         assertEquals(2, InMemoryHistoryManager.getHistoryStorage().getSize());
+    }
+
+    @Test
+    public void shouldRestoreEmptyEpic() throws IOException {
+        FileBackedTasksManager newTaskManager = new FileBackedTasksManager(dataFile, historyFile);
+        assertEquals(0, Files.size(dataFile.toPath()));
+        assertEquals(0, FileBackedTasksManager.getTaskManager().getTasksStorage().size());
+        newTaskManager.epicAdd(epic);
+        assertNotEquals(0, Files.size(dataFile.toPath()));
+
+        newTaskManager.restoreTasks(dataFile);
+        assertEquals(1, FileBackedTasksManager.getTaskManager().getTasksStorage().size());
     }
 
     @AfterEach
